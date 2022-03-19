34e55c8db3ab60fc66102db624c28d4e1e380256
younggyo <lee4519698@hanmail.net>
2022-03-19 22:19:07 +0900

    Query Method Basic

--- README.md
+++ README.md
@@ -2,11 +2,52 @@
 JPA 끄적끄적
 
 ---
-1. 필요 개념
-* getOne() vs findById()
-  * getOne() : 대상 Entity에 대한 Lazy Load. 객체의 속성에 엑세스할 필요 없는 경우.
-  * findById() : 주어진 ID에 대한 Entity 실제 로드. 모든 속성에 엑세스할 수 있도록 객체 로드.
+## 필요 개념
+### JPA CRUD
+* Entity
+  * DB에 저장하기 위해 유저가 정의한 클래스 -> Domain
+  * 일반적으로 RDBMS에서 Table을 객체화
 
-2. 실행 오류
+
+* JpaRepository interface 상속(Repository)
+  * 기본적으로 CRUD가 가능하도록 제공.
+  * Spring Data JPA에서ㅓ 제공하는 JpaRepository 인터페이스 상속 시, @Repository (x)
+  ```
+    public interface UserRepository extends JpaRepository<User, Long> {
+    * Generic <T, ID(PK 값)>
+        * T : Entity Type
+        * ID : 식별자 Type(PK)
+  }
+  ```
+* 주요 Method :
+  * getOne() vs findById()
+    * getOne() : 대상 Entity에 대한 Lazy Load. 객체의 속성에 엑세스할 필요 없는 경우.
+    * findById() : 주어진 ID에 대한 Entity 실제 로드. 모든 속성에 엑세스할 수 있도록 객체 로드.
+  * save(S) : 새로운 것 -> 저장, 이미 존재 -> 병합
+  * delete(T) : 삭제
+  * findAll : Sort or Pageable 조건을 파라미터로 제공할 수 있음.
+
+* [쿼리 메소드 필터 조건 공식 문서](https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation)
+  * Spring Data JPA -> Method name을 분석하여, JPQL을 생성 후 실행 -> 코드 간결
+  ![Query Method filter](./src/main/resources/static/QueryMethod.png)
+### Embedded H2
+* H2 Database : 자바 기반의 초 경량 RDBMS. 인 메모리 기반, JDBC API 지원, 브라우저에서 접속 가능한 콘솔 제공.
+* `application.yml` 설정
+  ```
+    spring:
+      h2:
+        console:
+          enabled: true  -> 콘솔 실행
+      jpa:
+        show-sql: true  -> 실행 sql문 로그
+        properties:
+          hibernate:
+            format_sql: true -> 로그 띄우기
+  ``` 
+* `intellij database` 설정
+  * server 실행 시 H2 URL 복사 -> 우측 상단 Database 탭 URL 복사
+  
+
+## 실행 오류
 * ['dataSourceScriptDatabaseInitializer' defined in class path resource](https://www.inflearn.com/questions/224708)
  : application.yml DB 테이블 자동 생성 오류, data.sql -> import.sql 파일 이름 변경. spring 2.5.0 버전부터 사용법 바뀜
--- src/test/java/com/example/springjpa/repository/UserRepositoryTest.java
+++ src/test/java/com/example/springjpa/repository/UserRepositoryTest.java
@@ -5,9 +5,11 @@ import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,19 +20,61 @@ class UserRepositoryTest {
 
     @Test
     void crud(){
-        // List<User> users = userRepository.findAll(Sort.by(Sort.Direction.DESC, "name"));
-//        List<User> users = userRepository.findAllById(Lists.newArrayList(1L, 3L, 5L));
-//        users.forEach(System.out::println);
+        userRepository.save(new User("david", "david@fastcampus.com"));
 
-        /*User user1 = new User("jack", "jack@naver.com");
-        User user2 = new User("steve", "steve@naver.com");
+        User user = userRepository.findById(1L).orElseThrow(RuntimeException::new);
+        user.setEmail("martin-updated@fastcampus.com");
 
-        userRepository.saveAll(Lists.newArrayList(user1, user2));
+        userRepository.save(user);
+    }
 
-        List<User> users = userRepository.findAll();
-        users.forEach(System.out::println);*/
+    @Test
+    void select(){
+//        System.out.println(userRepository.findByName("dennis"));
+//
+//        System.out.println("findByEmail : " + userRepository.findByEmail("martin@fastcampus.com"));
+//        System.out.println("getByEmail : " + userRepository.getByEmail("martin@fastcampus.com"));
+//        System.out.println("readByEmail : " + userRepository.readByEmail("martin@fastcampus.com"));
+//        System.out.println("queryByEmail : " + userRepository.queryByEmail("martin@fastcampus.com"));
+//        System.out.println("searchByEmail : " + userRepository.searchByEmail("martin@fastcampus.com"));
+//        System.out.println("streamByEmail : " + userRepository.streamByEmail("martin@fastcampus.com"));
+//        System.out.println("findUserByEmail : " + userRepository.findUserByEmail("martin@fastcampus.com"));
+//
+//        System.out.println("findSomethingByEmail : " + userRepository.findSomethingByEmail("martin@fastcampus.com"));
+//
+//        System.out.println("findTop2ByName : " + userRepository.findTop2ByName("martin"));
+//        System.out.println("findFirst2ByName : " + userRepository.findFirst2ByName("martin"));
+//        System.out.println("findLast1ByName : " + userRepository.findLast1ByName("martin"));
 
-        User user = userRepository.findById(1L).orElse(null);
-        System.out.println(user);
+        System.out.println("findByEmailAndName : " + userRepository.findByEmailAndName("martin@fastcampus.com", "martin"));
+        System.out.println("findByEmailOrName : " + userRepository.findByEmailOrName("martin@fastcampus.com", "dennis"));
+
+        System.out.println("findByCreatedAtAfter : " + userRepository.findByCreatedAtAfter(LocalDateTime.now().minusDays(1L)));
+        System.out.println("findByIdAfter : " + userRepository.findByIdAfter(4L));
+        System.out.println("findByCreatedAtGreaterThan : " + userRepository.findByCreatedAtGreaterThan(LocalDateTime.now().minusDays(1L)));
+        System.out.println("findByCreatedAtGreaterThanEqual : " + userRepository.findByCreatedAtGreaterThanEqual(LocalDateTime.now().minusDays(1L)));
+
+        System.out.println("findByCreatedAtBetween : " + userRepository.findByCreatedAtBetween(LocalDateTime.now().minusDays(1L), LocalDateTime.now().plusDays(1L)));
+        System.out.println("findByIdBetween : " + userRepository.findByIdBetween(1L, 3L));
+        System.out.println("findByIdGreaterThanEqualAndIdLessThanEqual : " + userRepository.findByIdGreaterThanEqualAndIdLessThanEqual(1L, 3L));
+
+        System.out.println("findByIdIsNotNull : " + userRepository.findByIdIsNotNull());
+//        System.out.println("findByIdIsNotEmpty : " + userRepository.findByAddressIsNotEmpty());
+
+        System.out.println("findByNameIn : " + userRepository.findByNameIn(Lists.newArrayList("martin", "dennis")));
+
+        System.out.println("findByNameStartingWith : " + userRepository.findByNameStartingWith("mar"));
+        System.out.println("findByNameEndingWith : " + userRepository.findByNameEndingWith("tin"));
+        System.out.println("findByNameContains : " + userRepository.findByNameContains("art"));
+
+        System.out.println("findByNameLike : " + userRepository.findByNameLike("%" + "art" + "%"));
+    }
+
+    @Test
+    void pagingAndSortingTest() {
+        System.out.println("findTopByName : " + userRepository.findTopByName("martin"));
+        System.out.println("findTopByName : " + userRepository.findFirstByName("martin", Sort.by(Sort.Order.desc("id"), Sort.Order.asc("email"))));
+        System.out.println("findTopByName : " + userRepository.findTopByName("lee", Sort.by(Sort.Order.desc("id"), Sort.Order.asc("email"))));
+        System.out.println("findByNameWithPaging : " + userRepository.findByName("lee", PageRequest.of(1, 1, Sort.by(Sort.Order.desc("id")))).getTotalElements());
     }
 }
